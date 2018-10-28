polymorphine/csrf

Extracted context libraries from Polymorphine/App package

src/Session/SessionData.php
@@ -1,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Context package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Context\Session;
+
+use Polymorphine\Context\Session;
+
+
+class SessionData
+{
+    private $manager;
+    private $data;
+
+    public function __construct(Session $manager, array $data = [])
+    {
+        $this->manager = $manager;
+        $this->data    = $data;
+    }
+
+    public function get(string $key, $default = null)
+    {
+        return $this->has($key) ? $this->data[$key] : $default;
+    }
+
+    public function set(string $key, $value): void
+    {
+        $this->data[$key] = $value;
+    }
+
+    public function has(string $key): bool
+    {
+        return array_key_exists($key, $this->data);
+    }
+
+    public function remove(string $key): void
+    {
+        unset($this->data[$key]);
+    }
+
+    public function clear(): void
+    {
+        $this->data = [];
+    }
+
+    public function commit(): void
+    {
+        $this->manager->commit($this->data);
+    }
+}

tests/CookieSetupTest.php
@@ -1,0 +1,146 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Context package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Context\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Polymorphine\Context\ResponseHeaders\CookieSetup;
+use Polymorphine\Context\Tests\Doubles\FakeResponseHeaders;
+use LogicException;
+
+require_once __DIR__ . '/Fixtures/time-functions.php';
+
+
+class CookieSetupTest extends TestCase
+{
+    /** @var FakeResponseHeaders */
+    private $headers;
+
+    public function testInstantiation()
+    {
+        $this->assertInstanceOf(CookieSetup::class, $this->cookie('new'));
+    }
+
+    /**
+     * @dataProvider cookieData
+     *
+     * @param string $headerLine
+     * @param array  $data
+     */
+    public function testCookieHeaders(string $headerLine, array $data)
+    {
+        $cookie = $this->cookie($data['name']);
+        isset($data['time']) and $cookie = $cookie->expires($data['time']);
+        isset($data['perm']) and $cookie = $cookie->permanent();
+        isset($data['domain']) and $cookie = $cookie->domain($data['domain']);
+        isset($data['path']) and $cookie = $cookie->path($data['path']);
+        isset($data['secure']) and $cookie = $cookie->secure();
+        isset($data['http']) and $cookie = $cookie->httpOnly();
+        isset($data['site']) and $cookie = $data['site'] ? $cookie->sameSiteStrict() : $cookie->sameSiteLax();
+
+        $data['value'] ? $cookie->value($data['value']) : $cookie->remove();
+        $this->assertEquals([$headerLine], $this->headers->data['Set-Cookie']);
+    }
+
+    public function cookieData()
+    {
+        return [
+            ['myCookie=; Path=/; Expires=Thursday, 02-May-2013 00:00:00 UTC; MaxAge=-157680000', [
+                'name'  => 'myCookie',
+                'value' => null
+            ]],
+            ['fullCookie=foo; Domain=example.com; Path=/directory/; Expires=Tuesday, 01-May-2018 01:00:00 UTC; MaxAge=3600; Secure; HttpOnly; SameSite=Lax', [
+                'name'   => 'fullCookie',
+                'value'  => 'foo',
+                'secure' => true,
+                'time'   => 60,
+                'http'   => true,
+                'domain' => 'example.com',
+                'path'   => '/directory/',
+                'site'   => false
+            ]],
+            ['permanentCookie=hash-3284682736487236; Expires=Sunday, 30-Apr-2023 00:00:00 UTC; MaxAge=157680000; HttpOnly; SameSite=Strict', [
+                'name'  => 'permanentCookie',
+                'value' => 'hash-3284682736487236',
+                'perm'  => true,
+                'http'  => true,
+                'path'  => '',
+                'site'  => true
+            ]]
+        ];
+    }
+
+    /**
+     * @dataProvider sameSiteDoubleCalls
+     *
+     * @param string $firstCall
+     * @param string $secondCall
+     */
+    public function testCookieWithSameSiteDirective_WhenSameSiteCalled_ThrowsException(string $firstCall, string $secondCall)
+    {
+        $cookie = $this->cookie('CheckLogic');
+        $cookie = ($firstCall === 'Lax') ? $cookie->sameSiteLax() : $cookie->sameSiteStrict();
+        $this->expectException(LogicException::class);
+        ($secondCall === 'Lax') ? $cookie->sameSiteLax() : $cookie->sameSiteStrict();
+    }
+
+    public function sameSiteDoubleCalls()
+    {
+        return [['Lax', 'Strict'], ['Strict', 'Lax'], ['Strict', 'Strict'], ['Lax', 'Lax']];
+    }
+
+    public function testSettingAttributesThroughConstructor()
+    {
+        $this->cookie('name', ['expires' => 65, 'sameSite' => 'something'])->value('value');
+        $headerLine = 'name=value; Path=/; Expires=Tuesday, 01-May-2018 01:05:00 UTC; MaxAge=3900; SameSite=Lax';
+        $this->assertEquals([$headerLine], $this->headers->data['Set-Cookie']);
+
+        $this->cookie('name', ['domain' => 'example.com', 'sameSite' => 'Strict'])->value('value');
+        $headerLine = 'name=value; Domain=example.com; Path=/; SameSite=Strict';
+        $this->assertEquals([$headerLine], $this->headers->data['Set-Cookie']);
+    }
+
+    public function testSecureAndHostNamePrefixWillSetSecureDirectiveImplicitly()
+    {
+        $cookie = $this->cookie('__SECURE-name');
+        $cookie->path('/test')->domain('example.com')->value('test');
+        $header = '__SECURE-name=test; Domain=example.com; Path=/test; Secure';
+        $this->assertSame([$header], $this->headers->data['Set-Cookie']);
+
+        $cookie = $this->cookie('__host-name');
+        $cookie->value('test');
+        $header = '__host-name=test; Path=/; Secure';
+        $this->assertSame([$header], $this->headers->data['Set-Cookie']);
+    }
+
+    public function testSettingPathForHostNamePrefixedCookie_ThrowsException()
+    {
+        $cookie = $this->cookie('__Host-name');
+        $this->expectException(LogicException::class);
+        $cookie->path('/test');
+    }
+
+    public function testSettingDomainForHostNamePrefixedCookie_ThrowsException()
+    {
+        $cookie = $this->cookie('__Host-name');
+        $this->expectException(LogicException::class);
+        $cookie->domain('example.com');
+    }
+
+    private function cookie(string $name, array $attributes = [], $resetHeaders = true)
+    {
+        $this->headers or $this->headers = new FakeResponseHeaders();
+        if ($resetHeaders) {
+            $this->headers->data = [];
+        }
+        return new CookieSetup($name, $this->headers, $attributes);
+    }
+}

tests/Doubles/FakeResponseHeaders.php
@@ -1,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Context package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Context\Tests\Doubles;
+
+use Polymorphine\Context\ResponseHeaders;
+use Polymorphine\Context\ResponseHeaders\CookieSetup;
+
+
+class FakeResponseHeaders implements ResponseHeaders
+{
+    public $data = [];
+
+    public function cookie(string $name, array $attributes = []): CookieSetup
+    {
+        return new CookieSetup($name, $this, $attributes);
+    }
+
+    public function add(string $name, string $header): void
+    {
+        $this->data[$name][] = $header;
+    }
+}

tests/Doubles/FakeStream.php
@@ -1,0 +1,98 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Context package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Context\Tests\Doubles;
+
+use Psr\Http\Message\StreamInterface;
+
+
+class FakeStream implements StreamInterface
+{
+    public $seekable = true;
+
+    private $body;
+    private $stream;
+
+    public function __construct(string $body = '')
+    {
+        $this->body   = $body;
+        $this->stream = $body;
+    }
+
+    public function __toString()
+    {
+        return $this->body;
+    }
+
+    public function close()
+    {
+    }
+
+    public function detach()
+    {
+    }
+
+    public function getSize()
+    {
+        return strlen($this->stream);
+    }
+
+    public function tell()
+    {
+    }
+
+    public function eof()
+    {
+        return empty($this->stream);
+    }
+
+    public function isSeekable()
+    {
+        return $this->seekable;
+    }
+
+    public function seek($offset, $whence = SEEK_SET)
+    {
+    }
+
+    public function rewind()
+    {
+        $this->stream = $this->body;
+    }
+
+    public function isWritable()
+    {
+    }
+
+    public function write($string)
+    {
+    }
+
+    public function isReadable()
+    {
+        return true;
+    }
+
+    public function read($length)
+    {
+        $send = substr($this->stream, 0, $length);
+        $this->stream = substr($this->stream, $length);
+        return $send;
+    }
+
+    public function getContents()
+    {
+    }
+
+    public function getMetadata($key = null)
+    {
+    }
+}

tests/Fixtures/SessionGlobalState.php
@@ -1,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Context package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Context\Tests\Fixtures;
+
+
+class SessionGlobalState
+{
+    public static $name   = 'PHPSESS';
+    public static $id     = '';
+    public static $status = PHP_SESSION_NONE;
+    public static $data   = [];
+
+    public static function reset()
+    {
+        self::$name   = 'PHPSESS';
+        self::$id     = '';
+        self::$status = PHP_SESSION_NONE;
+        self::$data   = [];
+    }
+}

tests/Fixtures/time-functions.php
@@ -1,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Context package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Context\ResponseHeaders;
+
+function time()
+{
+    return 1525132800;
+}

tests/SessionDataTest.php
@@ -1,0 +1,99 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Context package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Context\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Polymorphine\Context\Session\SessionData;
+use Polymorphine\Context\Tests\Doubles\FakeSession;
+
+
+class SessionDataTest extends TestCase
+{
+    public function testInstantiation()
+    {
+        $this->assertInstanceOf(SessionData::class, $session = $this->storage());
+    }
+
+    public function testGetData()
+    {
+        $storage = $this->storage(['foo' => 'bar']);
+        $this->assertSame('bar', $storage->get('foo'));
+    }
+
+    public function testSetData()
+    {
+        $storage = $this->storage();
+        $this->assertFalse($storage->has('foo'));
+        $storage->set('foo', 'bar');
+        $this->assertTrue($storage->has('foo'));
+        $this->assertSame('bar', $storage->get('foo'));
+    }
+
+    public function testSetOverwritesData()
+    {
+        $storage = $this->storage(['foo' => 'bar']);
+        $storage->set('foo', 'baz');
+        $this->assertSame('baz', $storage->get('foo'));
+    }
+
+    public function testRemoveData()
+    {
+        $storage = $this->storage(['foo' => 'bar', 'baz' => true]);
+        $storage->remove('foo');
+        $this->assertNull($storage->get('foo'));
+    }
+
+    public function testClearData()
+    {
+        $storage = new SessionData($manager = new FakeSession(), ['foo' => 'bar', 'baz' => true]);
+        $storage->clear();
+        $storage->commit();
+        $this->assertSame([], $manager->data);
+    }
+
+    public function testDefaultForMissingValues()
+    {
+        $storage = $this->storage();
+        $this->assertSame('default', $storage->get('foo', 'default'));
+    }
+
+    public function testCommitSession()
+    {
+        $data = [
+            'foo' => 'bar',
+            'bar' => 'baz'
+        ];
+
+        $storage = new SessionData($manager = new FakeSession(), $data);
+
+        $data['fizz'] = 'buzz';
+        $storage->set('fizz', 'buzz');
+
+        $storage->commit();
+        $this->assertSame($data, $manager->data);
+    }
+
+    public function testSettingNullDoesNotRemoveData()
+    {
+        $storage = new SessionData($manager = new FakeSession(), ['foo' => 500]);
+        $this->assertTrue($storage->has('foo'));
+        $storage->set('foo', null);
+        $this->assertTrue($storage->has('foo'));
+        $storage->commit();
+        $this->assertTrue(array_key_exists('foo', $manager->data));
+    }
+
+    private function storage(array $data = [], $manager = null): SessionData
+    {
+        return new SessionData($manager ?? new FakeSession(), $data);
+    }
+}

polymorphine/session

src/SessionContext/SessionData.php
@@ -1,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Session package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Session\SessionContext;
+
+use Polymorphine\Session\SessionContext;
+
+
+class SessionData
+{
+    private $manager;
+    private $data;
+
+    public function __construct(SessionContext $manager, array $data = [])
+    {
+        $this->manager = $manager;
+        $this->data    = $data;
+    }
+
+    public function get(string $key, $default = null)
+    {
+        return $this->has($key) ? $this->data[$key] : $default;
+    }
+
+    public function set(string $key, $value): void
+    {
+        $this->data[$key] = $value;
+    }
+
+    public function has(string $key): bool
+    {
+        return array_key_exists($key, $this->data);
+    }
+
+    public function remove(string $key): void
+    {
+        unset($this->data[$key]);
+    }
+
+    public function clear(): void
+    {
+        $this->data = [];
+    }
+
+    public function commit(): void
+    {
+        $this->manager->commit($this->data);
+    }
+}

tests/CookieSetupTest.php
@@ -1,0 +1,146 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Session package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Session\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Polymorphine\Session\ResponseHeaders\CookieSetup;
+use Polymorphine\Session\Tests\Doubles\FakeResponseHeaders;
+use LogicException;
+
+require_once __DIR__ . '/Fixtures/time-functions.php';
+
+
+class CookieSetupTest extends TestCase
+{
+    /** @var FakeResponseHeaders */
+    private $headers;
+
+    public function testInstantiation()
+    {
+        $this->assertInstanceOf(CookieSetup::class, $this->cookie('new'));
+    }
+
+    /**
+     * @dataProvider cookieData
+     *
+     * @param string $headerLine
+     * @param array  $data
+     */
+    public function testCookieHeaders(string $headerLine, array $data)
+    {
+        $cookie = $this->cookie($data['name']);
+        isset($data['time']) and $cookie = $cookie->expires($data['time']);
+        isset($data['perm']) and $cookie = $cookie->permanent();
+        isset($data['domain']) and $cookie = $cookie->domain($data['domain']);
+        isset($data['path']) and $cookie = $cookie->path($data['path']);
+        isset($data['secure']) and $cookie = $cookie->secure();
+        isset($data['http']) and $cookie = $cookie->httpOnly();
+        isset($data['site']) and $cookie = $data['site'] ? $cookie->sameSiteStrict() : $cookie->sameSiteLax();
+
+        $data['value'] ? $cookie->value($data['value']) : $cookie->remove();
+        $this->assertEquals([$headerLine], $this->headers->data['Set-Cookie']);
+    }
+
+    public function cookieData()
+    {
+        return [
+            ['myCookie=; Path=/; Expires=Thursday, 02-May-2013 00:00:00 UTC; MaxAge=-157680000', [
+                'name'  => 'myCookie',
+                'value' => null
+            ]],
+            ['fullCookie=foo; Domain=example.com; Path=/directory/; Expires=Tuesday, 01-May-2018 01:00:00 UTC; MaxAge=3600; Secure; HttpOnly; SameSite=Lax', [
+                'name'   => 'fullCookie',
+                'value'  => 'foo',
+                'secure' => true,
+                'time'   => 60,
+                'http'   => true,
+                'domain' => 'example.com',
+                'path'   => '/directory/',
+                'site'   => false
+            ]],
+            ['permanentCookie=hash-3284682736487236; Expires=Sunday, 30-Apr-2023 00:00:00 UTC; MaxAge=157680000; HttpOnly; SameSite=Strict', [
+                'name'  => 'permanentCookie',
+                'value' => 'hash-3284682736487236',
+                'perm'  => true,
+                'http'  => true,
+                'path'  => '',
+                'site'  => true
+            ]]
+        ];
+    }
+
+    /**
+     * @dataProvider sameSiteDoubleCalls
+     *
+     * @param string $firstCall
+     * @param string $secondCall
+     */
+    public function testCookieWithSameSiteDirective_WhenSameSiteCalled_ThrowsException(string $firstCall, string $secondCall)
+    {
+        $cookie = $this->cookie('CheckLogic');
+        $cookie = ($firstCall === 'Lax') ? $cookie->sameSiteLax() : $cookie->sameSiteStrict();
+        $this->expectException(LogicException::class);
+        ($secondCall === 'Lax') ? $cookie->sameSiteLax() : $cookie->sameSiteStrict();
+    }
+
+    public function sameSiteDoubleCalls()
+    {
+        return [['Lax', 'Strict'], ['Strict', 'Lax'], ['Strict', 'Strict'], ['Lax', 'Lax']];
+    }
+
+    public function testSettingAttributesThroughConstructor()
+    {
+        $this->cookie('name', ['expires' => 65, 'sameSite' => 'something'])->value('value');
+        $headerLine = 'name=value; Path=/; Expires=Tuesday, 01-May-2018 01:05:00 UTC; MaxAge=3900; SameSite=Lax';
+        $this->assertEquals([$headerLine], $this->headers->data['Set-Cookie']);
+
+        $this->cookie('name', ['domain' => 'example.com', 'sameSite' => 'Strict'])->value('value');
+        $headerLine = 'name=value; Domain=example.com; Path=/; SameSite=Strict';
+        $this->assertEquals([$headerLine], $this->headers->data['Set-Cookie']);
+    }
+
+    public function testSecureAndHostNamePrefixWillSetSecureDirectiveImplicitly()
+    {
+        $cookie = $this->cookie('__SECURE-name');
+        $cookie->path('/test')->domain('example.com')->value('test');
+        $header = '__SECURE-name=test; Domain=example.com; Path=/test; Secure';
+        $this->assertSame([$header], $this->headers->data['Set-Cookie']);
+
+        $cookie = $this->cookie('__host-name');
+        $cookie->value('test');
+        $header = '__host-name=test; Path=/; Secure';
+        $this->assertSame([$header], $this->headers->data['Set-Cookie']);
+    }
+
+    public function testSettingPathForHostNamePrefixedCookie_ThrowsException()
+    {
+        $cookie = $this->cookie('__Host-name');
+        $this->expectException(LogicException::class);
+        $cookie->path('/test');
+    }
+
+    public function testSettingDomainForHostNamePrefixedCookie_ThrowsException()
+    {
+        $cookie = $this->cookie('__Host-name');
+        $this->expectException(LogicException::class);
+        $cookie->domain('example.com');
+    }
+
+    private function cookie(string $name, array $attributes = [], $resetHeaders = true)
+    {
+        $this->headers or $this->headers = new FakeResponseHeaders();
+        if ($resetHeaders) {
+            $this->headers->data = [];
+        }
+        return new CookieSetup($name, $this->headers, $attributes);
+    }
+}

tests/Doubles/FakeResponseHeaders.php
@@ -1,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Session package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Session\Tests\Doubles;
+
+use Polymorphine\Session\ResponseHeaders;
+use Polymorphine\Session\ResponseHeaders\CookieSetup;
+
+
+class FakeResponseHeaders implements ResponseHeaders
+{
+    public $data = [];
+
+    public function cookie(string $name, array $attributes = []): CookieSetup
+    {
+        return new CookieSetup($name, $this, $attributes);
+    }
+
+    public function add(string $name, string $header): void
+    {
+        $this->data[$name][] = $header;
+    }
+}

tests/Doubles/FakeStream.php
@@ -1,0 +1,98 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Session package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Session\Tests\Doubles;
+
+use Psr\Http\Message\StreamInterface;
+
+
+class FakeStream implements StreamInterface
+{
+    public $seekable = true;
+
+    private $body;
+    private $stream;
+
+    public function __construct(string $body = '')
+    {
+        $this->body   = $body;
+        $this->stream = $body;
+    }
+
+    public function __toString()
+    {
+        return $this->body;
+    }
+
+    public function close()
+    {
+    }
+
+    public function detach()
+    {
+    }
+
+    public function getSize()
+    {
+        return strlen($this->stream);
+    }
+
+    public function tell()
+    {
+    }
+
+    public function eof()
+    {
+        return empty($this->stream);
+    }
+
+    public function isSeekable()
+    {
+        return $this->seekable;
+    }
+
+    public function seek($offset, $whence = SEEK_SET)
+    {
+    }
+
+    public function rewind()
+    {
+        $this->stream = $this->body;
+    }
+
+    public function isWritable()
+    {
+    }
+
+    public function write($string)
+    {
+    }
+
+    public function isReadable()
+    {
+        return true;
+    }
+
+    public function read($length)
+    {
+        $send = substr($this->stream, 0, $length);
+        $this->stream = substr($this->stream, $length);
+        return $send;
+    }
+
+    public function getContents()
+    {
+    }
+
+    public function getMetadata($key = null)
+    {
+    }
+}

tests/Fixtures/SessionGlobalState.php
@@ -1,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Session package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Session\Tests\Fixtures;
+
+
+class SessionGlobalState
+{
+    public static $name   = 'PHPSESS';
+    public static $id     = '';
+    public static $status = PHP_SESSION_NONE;
+    public static $data   = [];
+
+    public static function reset()
+    {
+        self::$name   = 'PHPSESS';
+        self::$id     = '';
+        self::$status = PHP_SESSION_NONE;
+        self::$data   = [];
+    }
+}

tests/Fixtures/time-functions.php
@@ -1,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Session package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Session\ResponseHeaders;
+
+function time()
+{
+    return 1525132800;
+}

tests/SessionDataTest.php
@@ -1,0 +1,99 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Session package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Session\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Polymorphine\Session\SessionContext\SessionData;
+use Polymorphine\Session\Tests\Doubles\FakeSessionContext;
+
+
+class SessionDataTest extends TestCase
+{
+    public function testInstantiation()
+    {
+        $this->assertInstanceOf(SessionData::class, $session = $this->storage());
+    }
+
+    public function testGetData()
+    {
+        $storage = $this->storage(['foo' => 'bar']);
+        $this->assertSame('bar', $storage->get('foo'));
+    }
+
+    public function testSetData()
+    {
+        $storage = $this->storage();
+        $this->assertFalse($storage->has('foo'));
+        $storage->set('foo', 'bar');
+        $this->assertTrue($storage->has('foo'));
+        $this->assertSame('bar', $storage->get('foo'));
+    }
+
+    public function testSetOverwritesData()
+    {
+        $storage = $this->storage(['foo' => 'bar']);
+        $storage->set('foo', 'baz');
+        $this->assertSame('baz', $storage->get('foo'));
+    }
+
+    public function testRemoveData()
+    {
+        $storage = $this->storage(['foo' => 'bar', 'baz' => true]);
+        $storage->remove('foo');
+        $this->assertNull($storage->get('foo'));
+    }
+
+    public function testClearData()
+    {
+        $storage = new SessionData($manager = new FakeSessionContext(), ['foo' => 'bar', 'baz' => true]);
+        $storage->clear();
+        $storage->commit();
+        $this->assertSame([], $manager->data);
+    }
+
+    public function testDefaultForMissingValues()
+    {
+        $storage = $this->storage();
+        $this->assertSame('default', $storage->get('foo', 'default'));
+    }
+
+    public function testCommitSession()
+    {
+        $data = [
+            'foo' => 'bar',
+            'bar' => 'baz'
+        ];
+
+        $storage = new SessionData($manager = new FakeSessionContext(), $data);
+
+        $data['fizz'] = 'buzz';
+        $storage->set('fizz', 'buzz');
+
+        $storage->commit();
+        $this->assertSame($data, $manager->data);
+    }
+
+    public function testSettingNullDoesNotRemoveData()
+    {
+        $storage = new SessionData($manager = new FakeSessionContext(), ['foo' => 500]);
+        $this->assertTrue($storage->has('foo'));
+        $storage->set('foo', null);
+        $this->assertTrue($storage->has('foo'));
+        $storage->commit();
+        $this->assertTrue(array_key_exists('foo', $manager->data));
+    }
+
+    private function storage(array $data = [], $manager = null): SessionData
+    {
+        return new SessionData($manager ?? new FakeSessionContext(), $data);
+    }
+}